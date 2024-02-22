47950fafa6eb622e5c471f7b1649e4bd1924f503
Malak <malakkjb@gmail.com>
2024-02-22 09:37:56 +0100

    modifications dans la branche feature notes

--- etudiant.java
+++ etudiant.java
@@ -2,16 +2,17 @@ public class Etudiant {
     // Attributs
     private String nom;
     private int numeroEtudiant;
-    
+    private double moyenne;
+    private double note;
 
     // Constructeur
-    public Etudiant(String nom, int numeroEtudiant) {
+    public Etudiant(String nom, int numeroEtudiant, double moyenne) {
         this.nom = nom;
         this.numeroEtudiant = numeroEtudiant;
-  
+        this.moyenne = moyenne;
     }
 
-    // Méthodes getters et setters
+    // Méthodes getters et setters pour nom, numéro étudiant, moyenne et note
     public String getNom() {
         return nom;
     }
@@ -28,10 +29,32 @@ public class Etudiant {
         this.numeroEtudiant = numeroEtudiant;
     }
 
+    public double getMoyenne() {
+        return moyenne;
+    }
+
+    public void setMoyenne(double moyenne) {
+        this.moyenne = moyenne;
+    }
+
+    public double getNote() {
+        return note;
+    }
+
+    public void setNote(double note) {
+        this.note = note;
+    }
+
+    // Méthode pour ajouter une note
+    public void ajouterNote(double nouvelleNote) {
+        this.note = nouvelleNote;
+    }
 
     // Méthode pour afficher les informations de l'étudiant
     public void afficherInformations() {
         System.out.println("Nom de l'étudiant : " + nom);
         System.out.println("Numéro d'étudiant : " + numeroEtudiant);
+        System.out.println("Moyenne de l'étudiant : " + moyenne);
+        System.out.println("Note de l'étudiant : " + note);
     }
 }
