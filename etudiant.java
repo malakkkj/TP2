f1697363ab70290099595c45c4c936d7b73d593d
Malak <malakkjb@gmail.com>
2024-02-22 09:32:52 +0100

    modifications dans la branche develop

--- etudiant.java
+++ etudiant.java
@@ -2,13 +2,13 @@ public class Etudiant {
     // Attributs
     private String nom;
     private int numeroEtudiant;
-    private double moyenne;
+    
 
     // Constructeur
-    public Etudiant(String nom, int numeroEtudiant, double moyenne) {
+    public Etudiant(String nom, int numeroEtudiant) {
         this.nom = nom;
         this.numeroEtudiant = numeroEtudiant;
-        this.moyenne = moyenne;
+  
     }
 
     // Méthodes getters et setters
@@ -28,18 +28,10 @@ public class Etudiant {
         this.numeroEtudiant = numeroEtudiant;
     }
 
-    public double getMoyenne() {
-        return moyenne;
-    }
-
-    public void setMoyenne(double moyenne) {
-        this.moyenne = moyenne;
-    }
 
     // Méthode pour afficher les informations de l'étudiant
     public void afficherInformations() {
         System.out.println("Nom de l'étudiant : " + nom);
         System.out.println("Numéro d'étudiant : " + numeroEtudiant);
-        System.out.println("Moyenne de l'étudiant : " + moyenne);
     }
 }
